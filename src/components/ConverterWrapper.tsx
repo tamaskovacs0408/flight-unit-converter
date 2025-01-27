c9e7fe06b03ab11a8a8f6a450f5291ba73340395
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-01-27 20:14:36 +0100

    add title prop to Converter components in ConverterWrapper for improved clarity

--- src/components/ConverterWrapper.tsx
+++ src/components/ConverterWrapper.tsx
@@ -1,15 +1,36 @@
 import Converter from "./Converter";
-import { metersConverter, kiloMetersConverter, speedConverter } from "../utils/converterFormules";
+import {
+  metersConverter,
+  kiloMetersConverter,
+  speedConverter,
+} from "../utils/converterFormules";
 
 function ConverterWrapper() {
-    return ( <section>
-        <h1>Flight Unit Converter</h1>
-        <div>
-            <Converter baseUnit="feet" transformedUnit="m" fn={metersConverter} />
-            <Converter baseUnit="nauticalMiles" transformedUnit="km" fn={kiloMetersConverter} />
-            <Converter baseUnit="knots" transformedUnit="km/h" fn={speedConverter} />
-        </div>
-    </section> );
+  return (
+    <section>
+      <h1>Flight Unit Converter</h1>
+      <div>
+        <Converter
+          title='Feet'
+          baseUnit='feet'
+          transformedUnit='m'
+          fn={metersConverter}
+        />
+        <Converter
+          title='Nautical Miles'
+          baseUnit='nauticalMiles'
+          transformedUnit='km'
+          fn={kiloMetersConverter}
+        />
+        <Converter
+          title='Knots'
+          baseUnit='knots'
+          transformedUnit='km/h'
+          fn={speedConverter}
+        />
+      </div>
+    </section>
+  );
 }
 
 export default ConverterWrapper;
