d04ef05bb99d662531329dbf1145a0b5e093aae2
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-01-27 20:23:10 +0100

    add eslint-plugin-react-compiler and update Vite config for improved React support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+const ReactCompilerConfig = { target: "19" };
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+  ],
+});
